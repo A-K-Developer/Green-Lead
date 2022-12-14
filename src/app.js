d4727d487b83b74498481db4ff552832cedd17e6
Asen K <krushkov1914@gmail.comgit$git config --global user.email krushkov1914@gmail.com>
2022-12-14 12:16:27 +0100

    STart

---
+++ src/app.js
@@ -0,0 +1,100 @@
+
+import { initializeApp } from "firebase/app"
+import {
+    onSnapshot,
+    getFirestore,
+    collection,
+    addDoc,
+    deleteDoc,
+    doc,
+    query,where,
+    orderBy,serverTimestamp,
+    getDoc,
+} from 'firebase/firestore'
+import './main.js';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBKt9WQuyMAostTmdvGwCvd3JwlVZQtZ3Y",
+    authDomain: "zeland-green-lead.firebaseapp.com",
+    projectId: "zeland-green-lead",
+    storageBucket: "zeland-green-lead.appspot.com",
+    messagingSenderId: "681778431372",
+    appId: "1:681778431372:web:e2cb9e3362b8e4aeac02a9"
+};
+
+// init firebase app
+initializeApp(firebaseConfig);
+
+//init services
+const db = getFirestore();
+
+// collection ref
+const colRef = collection(db, 'test');
+
+// queries
+
+// u can use where to find specific doc
+//  const q = query(colRef,where("title",'==','Echo 1'), orderBy('createdAt'))
+
+const q = query(colRef, orderBy('createdAt'))
+
+
+onSnapshot(q, (snapshot) => {
+    let tests = [];
+    snapshot.docs.forEach((doc) => {
+        tests.push({...doc.data(), id: doc.id})
+    })
+    //console.log(tests)
+
+})
+
+
+
+//get real-time collection data
+
+onSnapshot(colRef, (snapshot) => {
+    let tests = [];
+    snapshot.docs.forEach((doc) => {
+        tests.push({...doc.data(), id: doc.id})
+    })
+    //console.log(tests)
+
+})
+
+
+const addTestForm = document.querySelector('.add');
+addTestForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+    addDoc(colRef,{
+        title: addTestForm.title.value,
+        value: addTestForm.value.value,
+        createdAt: serverTimestamp()
+    })
+    .then(() => {
+        addTestForm.reset()
+    })
+})
+
+//deleting document
+const deleteTestForm = document.querySelector('.delete');
+deleteTestForm.addEventListener('submit',(e) => {
+    e.preventDefault()
+
+    const docRef = doc(db,'test',deleteTestForm.id.value)
+
+    deleteDoc(docRef)
+        .then(() => {
+            deleteTestForm.reset();
+        })
+})
+
+// get a single doc
+
+const docRef = doc( db , 'test', "F2gSCrB907HuoW98nV9N")
+
+
+onSnapshot(docRef, (doc) => {
+    //console.log(doc.data(), doc.id)
+})
+
+console.log('lqllql');
---
+++ src/main.js
@@ -0,0 +1,116 @@
+let main = document.querySelector('main');
+
+
+console.log('Hello Lavi');
+if(screen.width < 800){
+    console.log('Mobile lqlqllqlq')
+    let verticalDevice = createElement('div',main, 'verticalDevice','','','','','')
+    createElement('p',verticalDevice,'','','','','',`Hi.`)
+    createElement('p',verticalDevice,'','','','','',`For a better experience,please, turn your device horizontally.`)
+    let imgContainer = createElement('div',verticalDevice, 'imgContainer','','','','','')
+    createElement('img',imgContainer,'',['src','../img/mobile.png','alt','deviceImg'],'',['phone','phoneX'],'','')
+    createElement('img',imgContainer,'',['src','../img/1.png','alt','deviceImg'],'',['phone','phoneY'],'','')
+}else{
+    
+    createSlideShow();
+}   
+
+function createSlideShow(){
+    let frame = createElement('div',main,'frame','','','','','')
+    let content = createElement('div',frame,'content','','','','','')
+    let slideOne = createElement('div',content,'','','',['slide','one'],'','')
+
+    canvasforMobile(content)
+    
+    createElement('img',slideOne,'',['src','../img/Artboard3.png','alt','ImgMissing'],'',['windturbin'],'','',)
+    createElement('img',slideOne,'',['src','../img/Artboard4.png','alt','ImgMissing'],'',['windturbinSpin'],'','',)
+
+
+    let textContent = createElement('section',slideOne,'textContainer','','','','','');
+    createElement('h1',textContent,'','','',['heading'],'','Green Lead');
+    createElement('p',textContent,'','','','','','Guiding the green energy transition')
+
+    createElement('div',content,'','','',['slide','two'],'','')
+    createElement('div',content,'','','',['slide','three'],'','')
+}
+function canvasforMobile(main){
+    let slideFrameContainer = createElement('section',main,'frameContainer','','','','','');
+    let header = createElement('section',slideFrameContainer,'header','','','','','')
+    createElement('img',header,'logo',['src','../img/hand-cursor.png','alt','iconMissing-insta'],'',['icon'],'','')
+    createElement('button',header,'','','',['btn'],'','Portfolio')
+
+    let sideNav = createElement('section',slideFrameContainer,'sideNav','','','','','');
+    createElement('div',sideNav,'','','',['line', 'line1'],'','','',);
+    createElement('img',sideNav,'',['src','../img/gmail.png','alt','iconMissing-gmail'],'',['icon'],'','')
+    createElement('img',sideNav,'',['src','../img/insta.png','alt','iconMissing-insta'],'',['icon'],'','')
+    createElement('div',sideNav,'','','',['line', 'line2'],'','','');
+
+    let bottomNav = createElement('section',slideFrameContainer,'bottomNav','','','','','');
+    createElement('img',bottomNav,'',['src','../img/rightArrow.png','alt','iconMissing-insta'],'',['icon','leftArrow'],'','')
+    createElement('img',bottomNav,'',['src','../img/hand-cursor.png','alt','iconMissing-insta'],'',['icon'],'','')
+    createElement('img',bottomNav,'',['src','../img/rightArrow.png','alt','iconMissing-insta'],'',['icon'],'','')
+    createElement('div',bottomNav,'','','',['line', 'line3'],'','');
+
+    
+}
+
+/*
+    <div id="frame">
+        <div id="content">
+            <div class="slide one"></div>
+            <div class="slide two"></div>
+            <div class="slide three"></div>
+
+        </div>    
+    </div>
+ */
+function createElement(type, parent, id, attributeArr,
+    eventListenerArr, classArr, action, context,) {
+
+    let container = document.createElement(type);
+    
+    if(type == 'button'){
+        container.setAttribute('type','button')
+    }
+
+    if (context !== '') {
+        container.textContent = context;
+    }
+    if (id !== '') {
+        container.id = id;
+    }
+    while(attributeArr.length > 1){
+        let href = attributeArr.shift();
+        let path = attributeArr.shift();
+
+        if (href !== undefined && path !== undefined) {
+            container.setAttribute(href, path)
+        }
+    }
+   
+    while (classArr.length > 0 && classArr !== '') {
+        container.classList.add(classArr.shift())
+    }
+
+    if (eventListenerArr !== '') {
+        let event = eventListenerArr.shift();
+
+        while (eventListenerArr.length > 0) {
+            container.addEventListener(event, eventListenerArr[0]);
+            eventListenerArr.shift();
+        }
+    }
+
+    if (action == 'push') {
+        parent.push(container)
+    } else {
+        parent.appendChild(container)
+    }
+
+    return container
+}
+
+
+window.addEventListener('scroll', ()=>{
+    console.log('hiii');
+})
